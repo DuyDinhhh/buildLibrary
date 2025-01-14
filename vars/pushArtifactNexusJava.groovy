f5bca288680aa26bb4db929c2fe74593abad24a9
DuyDinhhh <161542772+DuyDinhhh@users.noreply.github.com>
2025-01-14 12:55:24 +0700

    Update pushArtifactNexusJava.groovy

--- vars/pushArtifactNexusJava.groovy
+++ vars/pushArtifactNexusJava.groovy
@@ -1,22 +1,20 @@
 def call() {
-    stage('Upload Artifact to Nexus'){
-        script {
-                def artifactPath = "target/${env.NEXUS_ARTIFACT_ID}-0.0.1.jar"
-                def artifactVersion = "${env.ARTIFACT_VERS}-${env.DEPLOY_TAG}"
-                nexusArtifactUploader(
-                    nexusVersion: 'nexus3',
-                    protocol: 'http',
-                    nexusUrl: env.NEXUS_URL,
-                    groupId: env.NEXUS_GROUP,
-                    version: artifactVersion,
-                    repository: env.NEXUS_REPOSITORY,
-                    credentialsId: env.NEXUS_CREDENTIALS_ID,
-                    artifacts: [
-                        [artifactId: env.NEXUS_ARTIFACT_ID,
-                         classifier: '',
-                         file: artifactPath,
-                         type: 'jar']
-                    ])
-        }
+    script {
+            def artifactPath = "target/${env.NEXUS_ARTIFACT_ID}-0.0.1.jar"
+            def artifactVersion = "${env.ARTIFACT_VERS}-${env.DEPLOY_TAG}"
+            nexusArtifactUploader(
+                nexusVersion: 'nexus3',
+                protocol: 'http',
+                nexusUrl: env.NEXUS_URL,
+                groupId: env.NEXUS_GROUP,
+                version: artifactVersion,
+                repository: env.NEXUS_REPOSITORY,
+                credentialsId: env.NEXUS_CREDENTIALS_ID,
+                artifacts: [
+                    [artifactId: env.NEXUS_ARTIFACT_ID,
+                     classifier: '',
+                     file: artifactPath,
+                     type: 'jar']
+                ])
     }
 }
